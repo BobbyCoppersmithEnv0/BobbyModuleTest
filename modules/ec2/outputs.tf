cd6d57c172b08d4031d178f92118e212790aeaa9
BobbyCoppersmithEnv0 <robert.coppersmith@env0.com>
2026-01-21 09:05:30 -0800

    Create outputs.tf

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = module.ec2.id
+}
+
+output "public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "arn" {
+  value = module.ec2.arn
+}
+
+output "ami" {
+  value = module.ec2.ami
+}
